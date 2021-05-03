551a426311fd502e0fd9b92a8d57f51158ec3b92
parazyd <parazyd@dyne.org>
2021-05-03 15:48:49 +0200

    WIP rewrite.
    Signed-off-by: parazyd <parazyd@dyne.org>

--- src/location-status.c
+++ src/location-status.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2020 Ivan J. <parazyd@dyne.org>
+ * Copyright (c) 2020-2021 Ivan J. <parazyd@dyne.org>
+ * Copyright (c) 2015      Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
  *
  * This file is part of location-status
  *
@@ -17,15 +18,9 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifdef HAVE_CONFIG_H
 #include <config.h>
-#endif
 
-#include <dbus/dbus.h>
 #include <dbus/dbus-glib-lowlevel.h>
-#include <gtk/gtk.h>
-#include <libintl.h>
-
 #include <hildon/hildon.h>
 #include <libhildondesktop/libhildondesktop.h>
 #include <libosso.h>
@@ -38,13 +33,19 @@ typedef struct _LocationStatusMenuItemPrivate LocationStatusMenuItemPrivate;
 struct _LocationStatusMenuItem
 {
 	HDStatusMenuItem parent;
-	LocationStatusMenuItemPrivate *prov;
+	LocationStatusMenuItemPrivate *priv;
 };
 
 struct _LocationStatusMenuItemClass
 {
 	HDStatusMenuItemClass parent;
 };
+
+typedef enum {
+	STATUS_ICON_NOT_CONNECTED,
+	STATUS_ICON_SEARCHING,
+	STATUS_ICON_FOUND
+} CurStatusIcon;
 
 struct _LocationStatusMenuItemPrivate
 {
@@ -56,131 +57,245 @@ struct _LocationStatusMenuItemPrivate
 	GdkPixbuf *pix16_gps_not_connected;
 	GdkPixbuf *pix48_gps_location;
 	GdkPixbuf *pix48_gps_not_connected;
-	LocationGPSDeviceMode searching;
-	GtkWidget *button;
+	int locationdaemon_running;
+	LocationGPSDeviceMode fix_status;
+	CurStatusIcon current_status_icon;
+	GtkWidget *status_button;
 };
 
 GType location_status_menu_item_get_type(void);
 
 HD_DEFINE_PLUGIN_MODULE_EXTENDED(LocationStatusMenuItem,
-		location_status_menu_item, HD_TYPE_STATUS_MENU_IDEM,
-		G_ADD_PRIVATE_DYNAMIC(LocationStatusMenuItem), , );
+	location_status_menu_item, HD_TYPE_STATUS_MENU_ITEM,
+	G_ADD_PRIVATE_DYNAMIC(LocationStatusMenuItem),,);
 
-static int execute_cp_plugin(GObject *object)
+#define GET_PRIVATE(x) location_status_menu_item_get_instance_private(x)
+
+static int execute_cp_plugin(gpointer obj)
 {
-    LocationStatusMenuItem *plugin = G_TYPE_CHECK_INSTANCE_CAST(
-            object, location_status_menu_item_get_type(),
-            LocationStatusMenuItem);
+	LocationStatusMenuItemPrivate *p = GET_PRIVATE(obj);
 
-	if (osso_cp_plugin_execute(plugin->priv->osso,
-				"liblocation_applet.so", object, TRUE) == -1)
-		g_warning("location-sb: Error starting location applet");
+	if (osso_cp_plugin_execute(p->osso, "liblocation_applet.so", obj, TRUE) == -1)
+		g_warning("location-sb: Error starting location cp applet");
+
 	return 0;
 }
 
-static void location_status_menu_item_init(LocationStatusMenuItem *plugin)
+static int fix_acquiring_cb(gpointer obj)
 {
-	plugin->priv = location_status_menu_item_get_instance_private(plugin);
-	LocationStatusMenuItemPrivate *priv = plugin->priv;
-	DBusError error;
+	LocationStatusMenuItemPrivate *p = GET_PRIVATE(obj);
 
+	if (p->fix_status != LOCATION_GPS_DEVICE_MODE_NO_FIX)
+		return 0;
+
+	switch (p->current_status_icon) {
+	case STATUS_ICON_SEARCHING:
+		set_status_icon(p->pix16_gps_location);
+		p->current_status_icon = STATUS_ICON_FOUND;
+		break;
+	case STATUS_ICON_FOUND:
+		set_status_icon(p->pix16_gps_searching);
+		p->current_status_icon = STATUS_ICON_SEARCHING;
+		break;
+	default:
+		break;
+	}
+
+	return 1;
+}
+
+static int fix_acquired(gpointer obj)
+{
+	LocationStatusMenuItemPrivate *p = GET_PRIVATE(obj);
+	set_status_icon(p->pix16_gps_location);
+	p->current_status_icon = STATUS_ICON_FOUND;
+	return 0;
+}
+
+static int handle_fixstatus(gpointer obj, DBusMessage *msg)
+{
+	LocationStatusMenuItemPrivate *p = GET_PRIVATE(obj);
+	short mode;
+
+	dbus_message_get_args(msg, NULL, DBUS_TYPE_BYTE, &mode, DBUS_TYPE_INVALID);
+
+	if (mode == p->fix_status)
+		return 1;
+
+	p->fix_status = mode;
+
+	/* Either static icon, or blink cb */
+	switch (mode) {
+	case LOCATION_GPS_DEVICE_MODE_NO_FIX:
+		g_timeout_add_seconds(1, fix_acquiring_cb, obj);
+		break;
+	case LOCATION_GPS_DEVICE_MODE_2D:
+	case LOCATION_GPS_DEVICE_MODE_3D:
+		fix_acquired(obj);
+		break;
+	default:
+		break;
+	};
+
+	return 1;
+}
+
+static int handle_running(gpointer obj, DBusMessage *msg)
+{
+	/* Either show or hide status icon */
+	LocationStatusMenuItemPrivate *p = GET_PRIVATE(obj);
+
+	dbus_message_get_args(msg, NULL, DBUS_TYPE_BYTE,
+		&p->locationdaemon_running, DBUS_TYPE_INVALID);
+
+	switch (p->locationdaemon_running) {
+	case 0:
+		/* Stopped */
+		break;
+	case 1:
+		/* Started */
+		break;
+	default:
+		break;
+	}
+
+	return 1;
+}
+
+static int on_locationdaemon_signal(int unused, DBusMessage *msg, gpointer obj)
+{
+	if (dbus_message_is_signal(msg, "org.maemo.LocationDaemon.Device",
+			"FixStatusChanged"))
+		return handle_fixstatus(obj, msg);
+
+	if (dbus_message_is_signal(msg, "org.maemo.LocationDaemon.Running",
+			"Running"))
+		return handle_running(obj, msg);
+
+	return 1;
+}
+
+static void location_status_menu_item_init(LocationStatusMenuItem *self)
+{
+	LocationStatusMenuItemPrivate *p = GET_PRIVATE(self);
 	GtkIconTheme *theme;
-	const gchar *btn_title;
-	GtkWidget *button_icon;
+	DBusError err;
 
-	dbus_error_init(&error);
+	dbus_error_init(&err);
 
-	priv->dbus = hd_status_plugin_item_get_dbus_connection(
-			DBUS_BUS_SYSTEM, &error);
+	p->locationdaemon_running = 0;
+	p->fix_status = LOCATION_GPS_DEVICE_MODE_NOT_SEEN;
 
-	if (dbus_error_is_set(&error)) {
-		g_warning("location-sb: Error getting bus: %s", error.message);
-		dbus_error_free(&error);
-	} else {
-		dbus_connection_setup_with_g_main(priv->dbus, NULL);
-		dbus_bus_add_match(priv->dbus,
-				"type='signal',interface='org.gpsd',member='fix'",
-				&error);
-		if (dbus_error_is_set(&error)) {
-			g_warning("location-sb: Failed to add match: %s", error.message);
-			dbus_error_free(&error);
-		}
+	p->dbus = hd_status_plugin_item_get_dbus_connection(
+		HD_STATUS_PLUGIN_ITEM(self),
+		DBUS_BUS_SYSTEM, &err);
 
-		if (!dbus_connection_add_filter(priv->dbus,
-					(DBusHandleMessageFunction)on_gpsd_signal,
-					plugin, NULL)) {
-			g_warning("location-sb: Failed to add filter");
-		}
+	if (dbus_error_is_set(&err)) {
+		g_warning("location-sb: Error getting dbus system bus: %s", err.message);
+		dbus_error_free(&err);
+		return;
+	}
+
+	dbus_connection_setup_with_g_main(p->dbus, NULL);
+
+	dbus_bus_add_match(p->dbus,
+		"type='signal',interface='org.maemo.LocationDaemon.Running',member='Running'",
+		&err);
+
+	if (dbus_error_is_set(&err)) {
+		g_warning("location-sb: Failed to add match: %s", err.message);
+		dbus_error_free(&err);
+		/* XXX: Clean up dbus */
+		return;
+	}
+
+	dbus_bus_add_match(p->dbus,
+		"type='signal',interface='org.maemo.LocationDaemon.Device',member='FixStatusChanged'",
+		&err);
+
+	if (dbus_error_is_set(&err)) {
+		g_warning("location-sb: Failed to add match: %s", err.message);
+		dbus_error_free(&err);
+		/* XXX: Clean up dbus */
+		return;
+	}
+
+	if (!dbus_connection_add_filter(p->dbus,
+			(DBusHandleMessageFunction)on_locationdaemon_signal,
+			self, NULL)) {
+		g_warning("location-sb: Failed to add dbus filter");
+		/* XXX: Clean up dbus */
+		return;
 	}
 
 	theme = gtk_icon_theme_get_default();
-	priv->pix16_gps_searching = gtk_icon_theme_load_icon(theme,
-			"gps_searching", 16, 0, NULL);
-	priv->pix16_gps_location = gtk_icon_theme_load_icon(theme,
-			"gps_location", 16, 0, NULL);
-	priv->pix16_gps_not_connected = gtk_icon_theme_load_icon(theme,
-			"gps_not_connected", 16, 0, NULL);
-	priv->pix48_gps_location = gtk_icon_theme_load_icon(theme,
-			"gps_location", 48, 0, NULL);
-	priv->pix48_gps_not_connected = gtk_icon_theme_load_icon(theme,
-			"gps_not_connected", 48, 0, NULL);
+	p->pix16_gps_searching = gtk_icon_theme_load_icon(theme,
+		"gps_searching", 16, 0, NULL);
+	p->pix16_gps_location = gtk_icon_theme_load_icon(theme,
+		"gps_location", 16, 0, NULL);
+	p->pix16_gps_not_connected = gtk_icon_theme_load_icon(theme,
+		"gps_not_not_connected", 16, 0, NULL);
+	p->pix48_gps_location = gtk_icon_theme_load_icon(theme,
+		"gps_not_connected", 48, 0, NULL);
 
-	priv->osso = osso_initialize("location-sb", "0.106", FALSE, NULL);
+	p->osso = osso_initialize("location-sb", "0.106", FALSE, NULL);
 
-	hd_status_plugin_item_set_status_area_icon(plugin, NULL);
+	hd_status_plugin_item_set_status_area_icon(
+		HD_STATUS_PLUGIN_ITEM(self), NULL);
 
-	btn_title = d_gettext("osso-location-ui", "loca_fi_status_menu_location");
-	priv->button = hildon_button_new_with_text(
-			HILDON_SIZE_FINGER_HEIGHT,
-			HILDON_BUTTON_ARRANGEMENT_VERTICAL,
-			btn_title, NULL);
+	p->status_button = hildon_button_new_with_text(
+		HILDON_SIZE_FINGER_HEIGHT,
+		HILDON_BUTTON_ARRANGEMENT_VERTICAL,
+		g_dgettext("osso-location-ui", "loca_fi_status_menu_location"),
+		NULL);
 
-	hildon_button_set_alignment(priv->button, 0.0, 0.5, 0.0, 0.0);
-	hildon_button_set_style(priv->button, HILDON_BUTTON_STYLE_PICKER);
-	button_icon = gtk_image_new_from_pixbuf(priv->pix48_gps_location);
-	hildon_button_set_image(priv->button, button_icon);
-	hildon_button_set_image_position(priv->button, 0);
+	hildon_button_set_alignment(HILDON_BUTTON(p->status_button),
+		0.0, 0.5, 0.0, 0.0);
 
-	g_signal_connect_data(priv->button, "clicked",
-			(GCallback)execute_cp_plugin, plugin, 0, 0);
+	hildon_button_set_style(HILDON_BUTTON(p->status_button),
+		 HILDON_BUTTON_STYLE_PICKER);
 
-	gtk_container_add(GTK_CONTAINER(plugin), priv->button);
-	gtk_widget_show_all(GTK_WIDGET(plugin));
+	hildon_button_set_image(HILDON_BUTTON(p->status_button),
+		gtk_image_new_from_pixbuf(p->pix48_gps_location));
 
-	// connect set_icon_if_visible to something
-	fix_status_changed(plugin);
+	hildon_button_set_image_position(HILDON_BUTTON(p->status_button),0);
+
+	g_signal_connect_data(p->status_button, "clicked",
+		G_CALLBACK(execute_cp_plugin), self, 0, 0);
+
+	gtk_container_add(GTK_CONTAINER(self), p->status_button);
+	gtk_widget_show_all(GTK_WIDGET(self));
 }
 
-static void location_status_menu_item_finalize(GObject *object)
+static void location_status_menu_item_finalize(gpointer obj)
 {
-	LocationStatusMenuItem *plugin = G_TYPE_CHECK_INSTANCE_CAST(
-			object, location_status_menu_item_get_type(),
-			LocationStatusMenuItem);
+	LocationStatusMenuItemPrivate *p = GET_PRIVATE(obj);
 
-	if (plugin->priv->osso)
-		osso_deinitialize(&plugin->priv->osso);
+	if (p->osso)
+		osso_deinitialize(p->osso);
 
-	if (plugin->priv->dbus) {
-		dbus_bus_remove_match(plugin->priv->dbus,
-				"type='signal',interface='org.gpsd',member='fix'"
-				NULL);
-		dbus_connection_remove_filter(plugin->priv->dbus,
-				(DBusHandleMessageFunction)on_gpsd_signal,
-				object);
-		dbus_connection_unref(plugin->priv->dbus);
-		plugin->priv->dbus = NULL;
+	if (p->dbus) {
+		dbus_bus_remove_match(p->dbus,
+			"type='signal',interface='org.maemo.LocationDaemon.Device',member='FixStatusChanged'",
+			NULL);
+		dbus_bus_remove_match(p->dbus,
+			"type='signal',interface='org.maemo.LocationDaemon.Running',member='Running'",
+			NULL);
+		dbus_connection_remove_filter(p->dbus,
+			(DBusHandleMessageFunction)on_locationdaemon_signal, obj);
+		dbus_connection_unref(p->dbus);
+		p->dbus = NULL;
 	}
 
-	G_OBJECT_CLASS(location_status_menu_item_parent_class)->finalize(object);
+	G_OBJECT_CLASS(location_status_menu_item_parent_class)->finalize(obj);
+}
+
+static void location_status_menu_item_class_init(LocationStatusMenuItemClass *klass)
+{
+	return;
 }
 
 static void location_status_menu_item_class_finalize(LocationStatusMenuItemClass *klass)
 {
 	return;
-}
-
-static void location_status_menu_item_class_init(LocationStatusMenuItemClass *klass)
-{
-	GObjectClass *object_class = G_OBJECT_CLASS(klass);
-	object_class->finalize = location_status_menu_item_class_finalize;
 }
